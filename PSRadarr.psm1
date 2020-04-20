e353380683b3b865115f043f961e55f6d6ac165d
Jonathan Moss <jwmoss88@gmail.com>
2020-04-19 20:19:50 -0400

    Added config and irm wrapper

--- PSRadarr.psm1
+++ PSRadarr.psm1
@@ -7,13 +7,48 @@ function Set-RadarrConfig {
         [string]
         $API
     )
+
+    $script:configuration = @{
+        URL = $URL
+        API = $API
+    }    
+
+}
+
+function Invoke-RadarrRestMethod {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet(
+            "GET",
+            "POST"
+            )]
+        [string]
+        $Method,
+
+        [string]
+        $Endpoint,
+
+        [string]
+        $Command
+    )
     
     begin {
-        
+        $InvokeRestMethodHash = @{
+            URI = $script:configuration.URL
+            Method = $Method
+            Headers = @{
+                'X-Api-Key' = $script:configuration.API
+            }
+        }
     }
     
     process {
+        if ($PSBoundParameters.ContainsKey('Command')) {
+            $InvokeRestMethodHash.Update("URI", $script:configuration.URL + $Endpoint + $Command)
+        }
         
+        Invoke-RestMethod @InvokeRestMethodHash 
     }
     
     end {
